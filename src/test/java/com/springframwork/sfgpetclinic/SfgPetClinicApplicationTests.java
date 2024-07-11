5e83237d35fba47bf0b8654aea65e60877b878ac
Srinivas P <srnvs0708@gmail.com>
2024-07-11 18:06:11 +0530

    Initial Commit

---
+++ src/test/java/com/springframwork/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframwork.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
