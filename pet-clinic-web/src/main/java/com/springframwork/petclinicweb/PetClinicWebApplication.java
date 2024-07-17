e380acce60243c073602a2550a606b24ed8d308b
Srinivas P <srnvs0708@gmail.com>
2024-07-17 22:43:38 +0530

    refactoring to multi module maven project. Closes #1.

---
+++ pet-clinic-web/src/main/java/com/springframwork/petclinicweb/PetClinicWebApplication.java
@@ -0,0 +1,13 @@
+package com.springframwork.petclinicweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicWebApplication.class, args);
+    }
+
+}
---
+++ pet-clinic-web/src/test/java/com/springframwork/petclinicweb/PetClinicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframwork.petclinicweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
