9db8067d920246c5591df38b78649644fb9f3723
Srinivas P <srnvs0708@gmail.com>
2024-07-18 16:31:43 +0530

    Changed the name of the web application.

--- pet-clinic-web/src/main/java/com/springframwork/petclinicweb/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframwork/petclinicweb/SfgPetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetClinicWebApplication {
+public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetClinicWebApplication.class, args);
+        SpringApplication.run(SfgPetClinicApplication.class, args);
     }
 
 }
