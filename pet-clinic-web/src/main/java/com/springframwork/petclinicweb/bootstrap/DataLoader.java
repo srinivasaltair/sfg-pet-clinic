0bcc0c2152c68535f1e23426422b329f9b6bde1d
Srinivas P <srnvs0708@gmail.com>
2024-07-31 12:24:45 +0530

    Added static files. Closes #17

--- pet-clinic-web/src/main/java/com/springframwork/petclinicweb/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframwork/petclinicweb/bootstrap/DataLoader.java
@@ -20,26 +20,22 @@ public class DataLoader implements CommandLineRunner {
 
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("John");
         owner1.setLastName("Doe");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Jane");
         owner2.setLastName("Doe");
         ownerService.save(owner2);
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("VJohn");
         vet1.setLastName("VDoe");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sam");
         vet2.setLastName("Axe");
         vetService.save(vet2);
