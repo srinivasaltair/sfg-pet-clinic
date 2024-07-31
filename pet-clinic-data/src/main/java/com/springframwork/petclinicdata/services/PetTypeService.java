7072dc7d1a68e7c04241a9964d5fb204e3c55857
Srinivas P <srnvs0708@gmail.com>
2024-08-01 00:54:47 +0530

    Create PetType map service. Closes #29

---
+++ pet-clinic-data/src/main/java/com/springframwork/petclinicdata/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframwork.petclinicdata.services;
+
+import com.springframwork.petclinicdata.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
