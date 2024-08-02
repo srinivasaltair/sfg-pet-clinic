ba778e25890aad86f19a41208cf0e4e83046738a
Srinivas P <srnvs0708@gmail.com>
2024-08-02 16:02:20 +0530

    Fixes broken links. Closes #47

--- pet-clinic-web/src/main/java/com/springframwork/petclinicweb/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframwork/petclinicweb/controller/OwnerController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
@@ -19,5 +20,10 @@ public class OwnerController {
 
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findHandler() {
+        return "notimplemented";
     }
 }
