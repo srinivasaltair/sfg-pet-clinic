d786bc5b97d5f8d36d0e0a47e7186df97ff8cb11
Srinivas P <srnvs0708@gmail.com>
2024-08-04 01:03:55 +0530

    Add springdata jpa for owners. Closes #40

--- pet-clinic-data/src/main/java/com/springframwork/petclinicdata/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/springframwork/petclinicdata/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.springframwork.petclinicdata.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
