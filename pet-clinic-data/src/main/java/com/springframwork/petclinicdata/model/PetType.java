719c117b1d90a929cee1abc9cafcc6f04cbcb9e5
Srinivas P <srnvs0708@gmail.com>
2024-08-04 00:33:27 +0530

    Convert Owner to JPA. Closes #36

--- pet-clinic-data/src/main/java/com/springframwork/petclinicdata/model/PetType.java
+++ pet-clinic-data/src/main/java/com/springframwork/petclinicdata/model/PetType.java
@@ -1,6 +1,13 @@
 package com.springframwork.petclinicdata.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
+    @Column(name = "name")
     private String name;
 
     public String getName() {
