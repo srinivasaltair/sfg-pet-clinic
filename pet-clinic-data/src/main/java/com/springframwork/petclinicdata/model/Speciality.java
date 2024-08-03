cfbd4b671934a7d3333303dbb79e321f6a9b7aba
Srinivas P <srnvs0708@gmail.com>
2024-08-04 00:38:31 +0530

    Convert Vets to JPA. Closes #37

--- pet-clinic-data/src/main/java/com/springframwork/petclinicdata/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springframwork/petclinicdata/model/Speciality.java
@@ -1,6 +1,14 @@
 package com.springframwork.petclinicdata.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
