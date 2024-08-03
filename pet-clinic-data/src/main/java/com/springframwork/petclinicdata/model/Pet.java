ad849aa9d11d1aada96ca398ee2ceb4d0adab889
Srinivas P <srnvs0708@gmail.com>
2024-08-04 00:43:09 +0530

    Convert Visits to JPA. Closes #38

--- pet-clinic-data/src/main/java/com/springframwork/petclinicdata/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframwork/petclinicdata/model/Pet.java
@@ -3,6 +3,8 @@ package com.springframwork.petclinicdata.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +23,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
